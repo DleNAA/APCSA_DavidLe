891eea32491cffd16ce67d23464ab963104ba0c3
David Le <daiduong.le@g.northernacademy.org>
2023-12-11 12:43:34 -0500

    update hw5

--- HW/HW5_P1_RPS.java
+++ HW/HW5_P1_RPS.java
@@ -10,23 +10,30 @@ public class HW5_P1_RPS {
         }
     }
     static int best_of() {
-        String input = s.nextLine();
-        try {
+        int ret_val = 0;
+        boolean valid = false;
+        while (!valid) {
             System.out.println("How many games (1, 3, 5): ");
-            int bo = Integer.parseInt(input);
-            switch (bo) {
-                case 1:
-                    return 1;
-                case 3:
-                    return 3;
-                case 5:
-                    return 5;
-                default:
-                    throw new Exception();
+            String input = s.nextLine();
+            try {
+                int bo = Integer.parseInt(input);
+                switch (bo) {
+                    case 1:
+                        valid = true;
+                        ret_val = 1;
+                    case 3:
+                        valid = true;
+                        ret_val = 3;
+                    case 5:
+                        valid = true;
+                        ret_val = 5;
+                    default:
+                        throw new Exception();
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid # of rounds!");
             }
-        } catch (Exception e) {
-            System.out.println("Invalid # of rounds!");
         }
-    
+        return ret_val;
     }
 }
